366a327aa767b265da2d1ef4e64566c0c1e68e03
Ted M. Young <tedyoung@gmail.com>
2021-04-06 15:33:20 -0700

    Lab 4A: Player beats dealer test with a StubDeck.

---
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
@@ -0,0 +1,22 @@
+package com.jitterted.ebp.blackjack.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class GameOutcomeTest {
+
+  @Test
+  public void playerBeatsDealer() throws Exception {
+    Deck stubDeck = new StubDeck(Rank.QUEEN, Rank.EIGHT,
+                                 Rank.TEN, Rank.JACK);
+    Game game = new Game(stubDeck);
+    game.initialDeal();
+
+    game.playerStands();
+    game.dealerTurn();
+
+    assertThat(game.determineOutcome())
+        .isEqualTo("You beat the Dealer! 💵");
+  }
+}
